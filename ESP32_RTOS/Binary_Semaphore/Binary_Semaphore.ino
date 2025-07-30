60cb4f9906dc85ad3e81e2e80e7994f54b724197
Ashwin <ashwinkumarh147@gmail.com>
2025-07-30 18:37:00 +0530

    add semaphore file

---
+++ ESP32_RTOS/Binary_Semaphore/Binary_Semaphore.ino
@@ -0,0 +1,44 @@
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+
+
+
+static const int led_pin = 2;
+static SemaphoreHandle_t bin_sem;
+
+void blinkLED(void *parameters) {
+  int num = *(int *)parameters;
+  xSemaphoreGive(bin_sem);
+  Serial.print("Received: ");
+  Serial.println(num);
+  pinMode(led_pin, OUTPUT);
+  while (1) {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(num / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(num / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  long int delay_arg;
+  Serial.begin(9600);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println("Enter a number for delay (milliseconds)");
+  while (Serial.available() <= 0);
+  delay_arg = Serial.parseInt();
+  Serial.print("Sending: ");
+  Serial.println(delay_arg);
+  bin_sem = xSemaphoreCreateBinary();
+  xTaskCreatePinnedToCore(blinkLED,"Blink LED",1024,(void *)&delay_arg,1,NULL,1);                   
+  xSemaphoreTake(bin_sem, portMAX_DELAY);
+  Serial.println("Done!");
+}
+
+void loop() {
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
